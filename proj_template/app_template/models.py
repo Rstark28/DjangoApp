4cb52dba9f0beb43cc7621c6f1079f086962e9d5
Matt Darin <matthewgdarin@gmail.com>
2024-06-29 11:54:15 -0400

    Added QB and game models

--- proj_template/app_template/models.py
+++ proj_template/app_template/models.py
@@ -30,6 +30,13 @@ class HistoricalData(models.Model):
     def __str__(self):
         return f"{self.date} - {self.team1} vs {self.team2}"
 
+class Quarterbacks(models.Model):
+    name = models.CharField(max_length=100)
+    QBR = models.FloatField()
+    
+    def __str__(self):
+        return self.name
+
 class NFLTeam(models.Model):
     team_name = models.CharField(max_length=100)
     abbreviation = models.CharField(max_length=3, unique=True)
@@ -40,3 +47,20 @@ class NFLTeam(models.Model):
 
     def __str__(self):
         return self.team_name
+
+class UpcomingGames(models.Model):
+    date = models.DateField()
+    season = models.IntegerField()
+    week = models.IntegerField()
+    after_bye_home = models.BooleanField()
+    after_bye_away = models.BooleanField()
+    city = models.CharField(max_length=100)
+    isNeutral = models.BooleanField()
+    homeTeam = models.ForeignKey(NFLTeam, related_name='home_teams', on_delete=models.CASCADE)
+    awayTeam = models.ForeignKey(NFLTeam, related_name='away_teams', on_delete=models.CASCADE)
+    isComplete = models.BooleanField()
+    homeScore = models.IntegerField()
+    awayScore = models.IntegerField()
+    def __str__(self):
+        return f"{self.date} - {self.awayTeam} @ {self.homeTeam}"
+
