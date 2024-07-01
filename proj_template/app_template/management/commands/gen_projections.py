fe0fa26a36314067caf2ee4d60d3fd08d61cc33d
Matt Darin <matthewgdarin@gmail.com>
2024-07-01 10:52:41 -0400

    Minor Change

--- proj_template/app_template/management/commands/gen_projections.py
+++ proj_template/app_template/management/commands/gen_projections.py
@@ -8,6 +8,8 @@ import pandas as pd
 import requests
 from datetime import datetime
 from datetime import date
+import pandas as pd
+import numpy as np
 
 class Command(BaseCommand):
     help = 'Updates/Generates the projections'
@@ -26,7 +28,8 @@ class Command(BaseCommand):
             awayElo = tracker[awayName]['Elo']
             homeElo = tracker[homeName]['Elo']
         
-        trackerDict = {team.team_name: {
+        #Created To Avoid Modifying the Team Class   
+        trackerList = {team.team_name: {
             'Elo': team.elo,
             'TotWins': team.totWins,
             'DivWins': team.divWins,
@@ -34,7 +37,21 @@ class Command(BaseCommand):
             'TeamsLostTo': [],
             'TeamsBeat': []
         } for team in teams}
+        
+        trackerDF = pd.DataFrame(columns=['Team', 'Elo', 'TotWins', 'DivWins', 'ConfWins', 'TeamsLostTo', 'TeamsBeat'])
+        trackerDF.set_index('Team', inplace=True)
+        for team in teams:
+            name = team.team_name
+            trackerDF.loc[name, 'Team'] = name
+            trackerDF.loc[name, 'Elo'] = team.elo
+            trackerDF.loc[name, 'ConfWins'] = team.confWins
+            trackerDF.loc[name, 'TotWins'] = team.totWins
+            trackerDF.loc[name, 'DivWins'] = team.divWins
+            trackerDF.loc[name, 'TeamsLostTo'] = ''
+            trackerDF.loc[name, 'TeamsBeat'] = ''
+        trackerDF.to_csv("test.csv", mode='w+', index=False)  
+        
         games = UpcomingGames.objects.all().filter(isComplete=False)
-        for i in range(17):
+        for i in range(1, 18):
             ...
         
