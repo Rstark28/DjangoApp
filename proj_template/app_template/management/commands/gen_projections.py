1853f35045bb767523fb73a040d792ff1d8961d0
Matt Darin <matthewgdarin@gmail.com>
2024-07-01 10:36:58 -0400

    Worked A Little On Generating Projections

--- proj_template/app_template/management/commands/gen_projections.py
+++ proj_template/app_template/management/commands/gen_projections.py
@@ -3,6 +3,7 @@ from app_template.models import NFLTeam
 from app_template.models import UpcomingGames
 from app_template.models import Season
 from app_template.models import Projection
+from app_template.models import City
 import pandas as pd
 import requests
 from datetime import datetime
@@ -13,7 +14,18 @@ class Command(BaseCommand):
 
         
     def handle(self, *args, **kwargs):
+        
         teams = NFLTeam.objects.all()
+        cities = City.objects.all()
+        
+        def getHomeOdds(Game: UpcomingGames, tracker: dict) -> float:
+            awayTeam = Game.awayTeam
+            homeTeam = Game.homeTeam
+            homeName = homeTeam.team_name
+            awayName = homeTeam.team_name
+            awayElo = tracker[awayName]['Elo']
+            homeElo = tracker[homeName]['Elo']
+        
         trackerDict = {team.team_name: {
             'Elo': team.elo,
             'TotWins': team.totWins,
