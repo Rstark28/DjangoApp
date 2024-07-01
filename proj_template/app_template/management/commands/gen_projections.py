038d97c3225c7c1ea4d6e0f065bc97435d6070e8
Matt Darin <matthewgdarin@gmail.com>
2024-07-01 14:20:33 -0400

    Worked more on projections

--- proj_template/app_template/management/commands/gen_projections.py
+++ proj_template/app_template/management/commands/gen_projections.py
@@ -11,6 +11,7 @@ from datetime import date
 import pandas as pd
 import numpy as np
 import math
+import random
 from geopy.distance import geodesic
 
 class Command(BaseCommand):
@@ -19,6 +20,8 @@ class Command(BaseCommand):
         
     def handle(self, *args, **kwargs):
         
+        
+        kFactor = 20
         teams = NFLTeam.objects.all()
         cities = City.objects.all()
         
@@ -58,19 +61,57 @@ class Command(BaseCommand):
             homeDiff += awayDistance * 4 / 1000
             homeOdds = 1/(10**(-1*homeDiff/400)+1)
             return homeOdds
-             
+        def addWin(winner: str, loser: str, df: pd.DataFrame, divisionDict: dict):
+            df.loc[winner, 'TotWins'] +=  1 
+            winnerDiv = divisionDict[winner]
+            loserDiv = divisionDict[loser]
+            if winnerDiv == loserDiv:
+                df.loc[winner, 'DivWins'] += 1
+                df.loc[winner, 'ConfWins'] += 1
+            elif winnerDiv.split()[0] == loserDiv.split()[0]:
+                df.loc[winner, 'ConfWins'] += 1
             
+            df.loc[winner, 'TeamsBeat'] += f";{loser}"
+            df.loc[loser, 'TeamsLostTo'] += f";{winner}"
             
+        def adjustElo(winner: str, loser: str, winnerOdds: float):
+            pass
+        divisionDict = {
+            "Buffalo Bills": "AFC East",
+            "Miami Dolphins": "AFC East",
+            "New England Patriots": "AFC East",
+            "New York Jets": "AFC East",
+            "Baltimore Ravens": "AFC North",
+            "Cincinnati Bengals": "AFC North",
+            "Cleveland Browns": "AFC North",
+            "Pittsburgh Steelers": "AFC North",
+            "Houston Texans": "AFC South",
+            "Indianapolis Colts": "AFC South",
+            "Jacksonville Jaguars": "AFC South",
+            "Tennessee Titans": "AFC South",
+            "Denver Broncos": "AFC West",
+            "Kansas City Chiefs": "AFC West",
+            "Las Vegas Raiders": "AFC West",
+            "Los Angeles Chargers": "AFC West",
+            "Dallas Cowboys": "NFC East",
+            "New York Giants": "NFC East",
+            "Philadelphia Eagles": "NFC East",
+            "Washington Commanders": "NFC East",
+            "Chicago Bears": "NFC North",
+            "Detroit Lions": "NFC North",
+            "Green Bay Packers": "NFC North",
+            "Minnesota Vikings": "NFC North",
+            "Atlanta Falcons": "NFC South",
+            "Carolina Panthers": "NFC South",
+            "New Orleans Saints": "NFC South",
+            "Tampa Bay Buccaneers": "NFC South",
+            "Arizona Cardinals": "NFC West",
+            "Los Angeles Rams": "NFC West",
+            "San Francisco 49ers": "NFC West",
+            "Seattle Seahawks": "NFC West"
+        }   
         
-        #Created To Avoid Modifying the Team Class   
-        trackerList = {team.team_name: {
-            'Elo': team.elo,
-            'TotWins': team.totWins,
-            'DivWins': team.divWins,
-            'ConfWins': team,
-            'TeamsLostTo': [],
-            'TeamsBeat': []
-        } for team in teams}
+        
         
         trackerDF = pd.DataFrame(columns=['Team', 'Elo', 'TotWins', 'DivWins', 'ConfWins', 'TeamsLostTo', 'TeamsBeat'])
         trackerDF.set_index('Team', inplace=True)
@@ -83,10 +124,25 @@ class Command(BaseCommand):
             trackerDF.loc[name, 'DivWins'] = team.divWins
             trackerDF.loc[name, 'TeamsLostTo'] = ''
             trackerDF.loc[name, 'TeamsBeat'] = ''
-        trackerDF.to_csv("test.csv", mode='w+', index=False)  
+        
         
         games = UpcomingGames.objects.all().filter(isComplete=False)
         for i in range(1, 18):
             weeklyGames = games.filter(week=i)
-            getHomeOdds(weeklyGames.first(), trackerDF)
+            for game in weeklyGames:
+                homeTeam = game.homeTeam
+                awayTeam = game.awayTeam
+                homeTeamName = homeTeam.team_name
+                awayTeamName = awayTeam.team_name
+                homeOdds = getHomeOddsStandard(game, trackerDF)
+                randNumber = random.random()
+                if randNumber < homeOdds:
+                    addWin(homeTeamName, awayTeamName, trackerDF, divisionDict)
+                    adjustElo(homeTeamName, awayTeamName, homeOdds)
+                else:
+                    addWin(awayTeamName, homeTeamName, trackerDF, divisionDict)
+                    adjustElo(awayTeamName, homeTeamName, 1 - homeOdds)
+                    
+        trackerDF.to_csv("test.csv", mode='w+', index=False)  
+                
         
